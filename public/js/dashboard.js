84eba33757340be163c9c6d983665c22fbae5cd0
Gsus-tech <chucho.moralitos0106@gmail.com>
2024-08-26 15:41:35 -0600

    Dashboard kanban funcional. Tiene funciones de drag and drop y un menu con las opciones para realizar el cambio de columna (estado). El contador de actividades se actualiza, el menu se cierra.

---
+++ public/js/dashboard.js
@@ -0,0 +1,207 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Seleccionar todos los botones con la clase .cardMenu
+    const cardMenuButtons = document.querySelectorAll('.cardMenu');
+    cardMenuButtons.forEach(button => {
+        button.addEventListener('click', function(event) {
+            event.stopPropagation();
+
+            // Crear el nuevo div para las opciones
+            const isMenuOn = document.querySelector('.opciones-tarjetas');
+            if (!isMenuOn) {
+                createDiv()
+            }else{
+                const oldMenu = document.querySelector('.opciones-tarjetas');
+                oldMenu.remove();
+                createDiv()
+            }
+
+            function createDiv(){
+                const actId = button.closest('.card-body').id;
+                const newMenu = document.createElement('div');
+                newMenu.className = 'opciones-tarjetas';
+                newMenu.id = actId;
+
+                const closestTaskDiv = button.closest('.tasks');
+                const columnId = closestTaskDiv ? closestTaskDiv.id : null;
+                const closestActDiv = button.closest('.card');
+                const cardId = closestActDiv ? closestActDiv.getAttribute('data-card-id') : null;
+
+                newMenu.innerHTML =  `
+                <a href="#" onclick="showTags(this, '${columnId}')" class="dropdown-item"><i class="mdi mdi-pencil me-1"></i>Etiquetas</a>
+                    <a href="#" onclick="showMoveOptions(this, '${columnId}', ${cardId})" class="dropdown-item"><i class="mdi mdi-pencil me-1"></i>Mover</a>
+                    <a href="#" onclick="confirmDeleteAct(this)" class="dropdown-item"><i class="mdi mdi-delete me-1"></i>Eliminar</a>
+                    `;
+
+                // Insertar el nuevo menú en el contenedor del botón
+                // const cardBody = button.closest('.card-body');
+                // cardBody.appendChild(newMenu);
+
+                document.body.appendChild(newMenu);  // Añadirlo al body
+
+                const rect = button.getBoundingClientRect();  // Obtener posición del botón
+                newMenu.style.top = `${rect.bottom}px`;  // Posicionarlo justo debajo del botón
+                newMenu.style.left = `${rect.left}px`;
+
+                // Evento de clic para cerrar el menú
+                document.addEventListener('click', function closeMenu(event) {
+                    if (!newMenu.contains(event.target) && event.target !== button) {
+                        newMenu.remove();  // Eliminar el menú si se hace clic fuera del Div
+                        document.removeEventListener('click', closeMenu);
+                    }
+                });
+            }
+        });
+    });
+});
+
+function showMoveOptions(button, columnId, cardId) {
+    event.stopPropagation(); // Esto evita que se ejecute el evento de cierre del div
+    const menu = button.closest('.opciones-tarjetas');
+
+    // Verificacion del elemento menu
+    if (menu) {
+
+        // Dependiendo del id del div 'tasks', mostrar un HTML distinto
+        let menuOptionsHTML = '';
+
+        if (columnId === 'pendientes') {
+            menuOptionsHTML = `
+                <a href="#" onclick="moveCard(2, '${cardId}')" class="dropdown-item"><i class="mdi mdi-pencil me-1"></i>En proceso</a>
+                <a href="#" onclick="moveCard(3, '${cardId}')" class="dropdown-item"><i class="mdi mdi-pencil me-1"></i>Retrasadas</a>
+                <a href="#" onclick="moveCard(4, '${cardId}')" class="dropdown-item"><i class="mdi mdi-delete me-1"></i>Terminadas</a>
+            `;
+        } else if (columnId === 'proceso') {
+            menuOptionsHTML = `
+                <a href="#" onclick="moveCard(1, '${cardId}')" class="dropdown-item"><i class="mdi mdi-pencil me-1"></i>Pendiente</a>
+                <a href="#" onclick="moveCard(3, '${cardId}')" class="dropdown-item"><i class="mdi mdi-pencil me-1"></i>Retrasadas</a>
+                <a href="#" onclick="moveCard(4, '${cardId}')" class="dropdown-item"><i class="mdi mdi-delete me-1"></i>Terminadas</a>
+            `;
+        } else if (columnId === 'retrasadas') {
+            menuOptionsHTML = `
+                <a href="#" onclick="moveCard(1, '${cardId}')" class="dropdown-item"><i class="mdi mdi-pencil me-1"></i>Pendiente</a>
+                <a href="#" onclick="moveCard(2, '${cardId}')" class="dropdown-item"><i class="mdi mdi-pencil me-1"></i>En proceso</a>
+                <a href="#" onclick="moveCard(4, '${cardId}')" class="dropdown-item"><i class="mdi mdi-delete me-1"></i>Terminadas</a>
+            `;
+        } else if (columnId === 'terminadas') {
+            menuOptionsHTML = `
+                <a href="#" onclick="moveCard(1, '${cardId}')" class="dropdown-item"><i class="mdi mdi-pencil me-1"></i>Pendiente</a>
+                <a href="#" onclick="moveCard(2, '${cardId}')" class="dropdown-item"><i class="mdi mdi-pencil me-1"></i>En proceso</a>
+                <a href="#" onclick="moveCard(3, '${cardId}')" class="dropdown-item"><i class="mdi mdi-delete me-1"></i>Retrasada</a>
+            `;
+        }
+        // Reemplazar el contenido del menú actual con las nuevas opciones
+        menu.innerHTML = menuOptionsHTML;
+    }
+}
+
+
+function showTags(button){
+    event.stopPropagation(); // Esto evita que se ejecute el evento de cierre del div
+    const menu = button.closest('.opciones-tarjetas');
+
+    // Verificacion del elemento menu
+    if (menu) {
+        // Dependiendo del id del div 'tasks', mostrar un HTML distinto
+        let menuOptionsHTML = '';
+
+        menuOptionsHTML = `
+        <a href="#" onclick="cardFlags(this, 2)" class="dropdown-item"><i class="mdi mdi-pencil me-1 fa fa-circle rdFlag"></i>  Importante / Urgente</a>
+        <a href="#" onclick="cardFlags(this, 3)" class="dropdown-item"><i class="mdi mdi-pencil me-1 fa fa-circle grFlag"></i>  Completado o bajo control</a>
+        <a href="#" onclick="cardFlags(this, 4)" class="dropdown-item"><i class="mdi mdi-delete me-1 fa fa-circle ylFlag"></i>  Información adicional requerida</a>
+        `;
+
+        // Reemplazar el contenido del menú actual con las nuevas opciones
+        menu.innerHTML = menuOptionsHTML;
+    }
+}
+
+
+function confirmDeleteAct(element){
+    if(confirm('¿Estas seguro de querer eliminar esta actividad?')){
+            //Codigo para eliminar una tarea.
+    }
+}
+
+0
+function moveCard(targetColumnId, cardId) {    
+    const nm = {
+        1: "one",
+        2: "two",
+        3: "three",
+        4: "four"
+    };
+
+    const idString = `task-list-${nm[targetColumnId]}`;
+    const cardIdString = `[data-card-id="${cardId}"]`;
+    const card = document.querySelector(cardIdString);
+    const targetColumn = document.getElementById(idString);
+    if (card && targetColumn) {
+        //Info de columna origen
+        const sourceColumn = card.closest('.task-list-items');
+        const sourceColumnId = sourceColumn.id;
+
+        targetColumn.appendChild(card);
+        
+        //Actualizar datos columna destino
+        actualizarContador(idString);
+        
+        //Actualizar datos columna origen
+        actualizarContador(sourceColumnId);
+
+
+        // Codigo para actualizar en la base de datos
+        // updateCardColumn(cardId, targetColumnId);
+    }
+}
+
+function updateCardColumn(button, cardId, targetColumnId) {
+    fetch('updateCardColumn.php', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: `cardId=${encodeURIComponent(cardId)}&columnId=${encodeURIComponent(targetColumnId)}`
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.success) {
+            console.log('Tarjeta actualizada correctamente en la base de datos.');
+        } else {
+            console.error('Error al actualizar la tarjeta:', data.message);
+        }
+    })
+    .catch(error => {
+        console.error('Error en la solicitud AJAX:', error);
+    });
+}
+
+function actualizarContador(columnId) {
+    const column = document.getElementById(`${columnId}`);
+    const noTasksMessage = column.querySelector('.emptyTaskDivMsj');
+    const hasTasks = column.querySelector('.card');
+    
+    //Remover o agregar el mensaje de Sin tareas registradas...
+    if (hasTasks) {
+        if (noTasksMessage) {
+            noTasksMessage.remove();
+            const taskContainer = column.closest('.task-list-items');
+                taskContainer.style.border = 'none';
+        }
+    } else {
+        console.log('No non-task message found  .');
+        if (!noTasksMessage) {
+            const placeholder = document.createElement('div');
+                placeholder.className = 'emptyTaskDivMsj';
+                placeholder.textContent = 'Sin tareas...';
+                column.appendChild(placeholder);
+                const taskContainer = column.closest('.task-list-items');
+                taskContainer.style.border = '1px dashed #ccc';
+        }
+    }
+
+    //Contador de actividades en la columna
+    const titleElement = column.closest('.tasks').querySelector('.task-header');
+    const cardCount = column.querySelectorAll('.card').length;
+    const [columnName] = titleElement.textContent.split(' (');
+    titleElement.textContent = `${columnName} (${cardCount})`;
+}
---
+++ public/js/ui/component.dragula.js
@@ -0,0 +1,93 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    const containers = [
+        document.getElementById('task-list-one'),
+        document.getElementById('task-list-two'),
+        document.getElementById('task-list-three'),
+        document.getElementById('task-list-four')
+    ];
+
+    const drake = dragula(containers, {
+        accepts: function(el, target, source, sibling) {
+            // Permitir soltar en contenedores vacíos
+            return true;
+        },
+        moves: function (el, source, handle, sibling) {
+            return true; // Permitir que todos los elementos se puedan arrastrar
+        }
+    });
+    
+    // Revisar y agregar placeholders a contenedores vacíos
+    drake.on('drop', function(el, target, source, sibling) {
+        checkForEmptyContainers(containers);
+
+        //Codigo para actualizar la posicion de la actividad en la BD
+        actualizarContador(target.id);
+        actualizarContador(source.id);
+    });
+
+    function checkForEmptyContainers(containers) {
+        containers.forEach(container => {
+            const hasPlaceholder = container.querySelector('.emptyTaskDivMsj');
+            const childCount = Array.from(container.children).filter(child => !child.classList.contains('emptyTaskDivMsj')).length;
+    
+            if (childCount === 0 && !hasPlaceholder) {
+                // Add placeholder if the container is empty
+                const placeholder = document.createElement('div');
+                placeholder.className = 'emptyTaskDivMsj';
+                placeholder.textContent = 'Sin tareas...';
+                container.appendChild(placeholder);
+    
+                // Optionally, add a border
+                const taskContainer = container.closest('.task-list-items');
+                if (taskContainer) {
+                    taskContainer.style.border = '1px dashed #ccc';
+                }
+            } else if (childCount > 0 && hasPlaceholder) {
+                // Remove placeholder if the container is not empty
+                hasPlaceholder.remove();
+                const taskContainer = container.closest('.task-list-items');
+                if (taskContainer) {
+                    taskContainer.style.border = '';  // Remove border
+                }
+            }
+        });
+    }    
+
+    // Verificar los contenedores al cargar la página
+    checkForEmptyContainers(containers);
+
+});
+
+
+
+//Auto scroll - todavia no esta terminada.
+
+let autoScrollInterval;
+
+function startAutoScroll(e) {
+    const scrollMargin = 200; 
+    const scrollSpeed = 20;  
+
+    
+    if (e.clientY < scrollMargin) {
+        autoScrollInterval = setInterval(() => {
+            window.scrollBy(0, -scrollSpeed); 
+        }, 50);
+    } else if (e.clientY > window.innerHeight - scrollMargin) {
+        autoScrollInterval = setInterval(() => {
+            window.scrollBy(0, scrollSpeed); 
+        }, 50);
+    } else {
+        clearInterval(autoScrollInterval);
+    }
+}
+
+// Escuchar el evento de drag
+document.addEventListener('drag', startAutoScroll);
+
+// Detener el scroll automático cuando se suelta el elemento arrastrado
+document.addEventListener('dragend', () => {
+    clearInterval(autoScrollInterval);
+});
+
